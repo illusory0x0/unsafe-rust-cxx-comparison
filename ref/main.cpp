a7379d41e930d7b92ac87ec8a10be7a025abe73b
猗露 <69190413+illusory0x0@users.noreply.github.com>
2026-01-01 18:51:21 +0800

    update

--- ref/main.cpp
+++ ref/main.cpp
@@ -5,7 +5,7 @@ int main() {
     int& rx = x; 
     int* px = reinterpret_cast<int*>(rx);
     // rx 是 lvalue, 所以仅仅只是把 rx 解引用后的值，coerce 成 int*
-    // https://en.cppreference.com/w/cpp/language/reinterpret_cast.html
+    // 6): https://en.cppreference.com/w/cpp/language/reinterpret_cast.html
 
     std::cout << "x: " <<  x << '\n';
     std::cout << "rx: " <<  rx << '\n';
