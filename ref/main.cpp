638438811f5fe29eaaea3f4ad9cb4e8b86891aaf
猗露 <69190413+illusory0x0@users.noreply.github.com>
2026-01-01 18:47:37 +0800

    fix errors

--- ref/main.cpp
+++ ref/main.cpp
@@ -4,6 +4,8 @@ int main() {
     int x = 1;
     int& rx = x; 
     int* px = reinterpret_cast<int*>(rx);
+    // rx 是 lvalue, 所以仅仅只是把 rx 解引用后的值，coerce 成 int*
+    // https://en.cppreference.com/w/cpp/language/reinterpret_cast.html
 
     std::cout << "x: " <<  x << '\n';
     std::cout << "rx: " <<  rx << '\n';
--- ref/main.rs
+++ ref/main.rs
@@ -2,6 +2,7 @@ fn main() {
   let x : i32 = 1;
   let rx : &i32 = &x; 
   let px : *const i32 = unsafe { 
+    // Rust 并没有解引用，&直接*把引用解释成指针。
     std::mem::transmute(rx)
   } ;
   println!("x: {}",x);
